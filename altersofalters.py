9ad81419f580759cac4e462b3ff48394464312c1
Ekaterina <44580299+katyalex@users.noreply.github.com>
2019-03-28 01:00:01 +0900

    Update altersofalters.py

--- altersofalters.py
+++ altersofalters.py
@@ -7,12 +7,13 @@ Created on Wed Mar 27 19:53:11 2019
 
 import pandas as pd
 import os 
-
+import numpy as np
 
 os.chdir('D:\\Yonsei\\2018년 2학기\\Mortality and providing social support')
-table = pd.read_stata('w1.dta') 
-newdf = table[['nid', 'n_nid_1','n_nid_2','n_nid_3', 'n_nid_4', 'n_nid_5', 'n_nid_6', 'n_nid_7','wave','komsano', 'n_komsa_1', 'n_komsa_2','n_komsa_3','n_komsa_4','n_komsa_5','n_komsa_6','n_komsa_7']]
-
+table = pd.read_stata('w1.dta') #데이터 읽기
+nids = table[['nid', 'n_nid_1','n_nid_2','n_nid_3', 'n_nid_4', 'n_nid_5', 'n_nid_6', 'n_nid_7','wave','komsano', 'n_komsa_1', 'n_komsa_2','n_komsa_3','n_komsa_4','n_komsa_5','n_komsa_6','n_komsa_7', 'health_beh']]
+newdf = nids
+komsa = table[[]]
 
 
 df = pd.DataFrame({'nid': [], 'n_nid_1': [], 'n_nid_2': [],'n_nid_3': [], 'n_nid_4': [], 'n_nid_5': [],'n_nid_6': [],'n_nid_7': [], 'n_nid_11': [],  'n_nid_12': [],  'n_nid_13': [],  'n_nid_14': [],  'n_nid_15': [],  'n_nid_16': [], 
@@ -33,41 +34,43 @@ dfkomsa = pd.DataFrame({'nid': [], 'n_nid_1': [], 'n_nid_2': [],'n_nid_3': [], '
 
 for index,row in newdf.iterrows():
     ego = int(row['nid'])
-    komsa = int(row['komsano'])
+    komsa = int(row['health_beh'])
     number = row.name
     df.set_value(number, 'nid', ego)
     dfkomsa.set_value(number, 'nid', komsa)
     for i in range(1,8):
         try:
             alter= int(row['n_nid_'+str(i)])
-            alterkomsa = int(row['n_komsa_'+str(i)])
+            #alterkomsa = int(row['n_komsa_'+str(i)])
             try:
                 num = newdf[newdf['nid']==alter].index.item()
                 newego = int(newdf.iloc[num]['nid'])
-                newkomsa = int(newdf.iloc[num]['n_komsa_'+str(i)])
+                newkomsa = int(newdf.iloc[num]['health_beh'])
                 df.set_value(number, 'n_nid_'+str(i), newego)
                 dfkomsa.set_value(number,'n_nid_'+str(i),newkomsa )
                 for k in range(1, 8):
                         try:
                             newalter = int(newdf.iloc[num]['n_nid_'+str(k)])
+                            numnew = newdf[newdf['nid']==newalter].index.item()
                             if newalter==ego:
                                 k=+1
                             else:
-                                newalterkomsa= int(newdf.iloc[num]['n_komsa_'+str(k)])
+                                newalterkomsa= int(newdf.iloc[numnew]['health_beh'])
                                 df.set_value(number, 'n_nid_'+str(i)+str(k), newalter)
                                 dfkomsa.set_value(number,'n_nid_'+str(i)+str(k),newalterkomsa )
                         except ValueError:
                                 k=+1
             except ValueError:
                 df.set_value(number, 'n_nid_'+str(i), alter)
-                dfkomsa.set_value(number,'n_nid_'+str(i),alterkomsa)
-                for k in range(1, 7):
+                #dfkomsa.set_value(number,'n_nid_'+str(i),alterkomsa)
+                for k in range(1, 8):
                         try:
                             newalter = int(newdf.iloc[num]['n_nid_'+str(k)])
+                            numnew = newdf[newdf['nid']==newalter].index.item()
                             if newalter==ego:
                                 k=+1
                             else:
-                                newalterkomsa= int(newdf.iloc[num]['n_komsa_'+str(k)])
+                                newalterkomsa= int(newdf.iloc[numnew]['health_beh'])
                                 df.set_value(number, 'n_nid_'+str(i)+str(k), newalter)
                                 dfkomsa.set_value(number,'n_nid_'+str(i)+str(k),newalterkomsa )
                         except ValueError:
@@ -75,4 +78,24 @@ for index,row in newdf.iterrows():
         except ValueError:
             i=+1
 
+rows = []
+healths = []
+for index, row in df.iterrows():
+    row1 = row
+    number = row.name
+    newrow = row1.drop_duplicates()
+    indices = list(newrow.index.values)
+    newdfkomsa = df.drop(indices, axis=1)
+    newind = list(newdfkomsa.columns.values)
+    dfkomsa.set_value(number,newind,np.nan )
+    df.set_value(number, newind, np.nan)
+    #newhealth = dfkomsa.iloc[number][indices]
+    rows.append(newrow)
+    #healths.append(newhealth)
+    row=+1
+    
+
 df.to_csv('altersofalters.csv', index=False)    
+dfkomsa.to_csv('komsaalters.csv', index=False)
+
+
